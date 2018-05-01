104c388c4d1523d9eea6f842c605868fe050d0a5
echo3582 <qianlu3582@163.com>
2018-05-01 19:51:51 +0800

    docs&&style: 1.编写readme文档 2.优化app.js格式

--- js/app.js
+++ js/app.js
@@ -10,18 +10,18 @@ class Enemy {
         this.x = x;
         this.y = y;
         this.speed = 100;
-    // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
+        // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
         this.sprite = 'images/enemy-bug.png';
     }
     // 此为游戏必须的函数，用来更新敌人的位置
     // 参数: dt ，表示时间间隙
     update(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
+        // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+        // 都是以同样的速度运行的
         this.x += this.speed * dt;
         console.log(`playerLocation:${player.x},${player.y}`);
-    //碰撞检测
-        if(player.x < this.x + WIDTH/2 && player.x + WIDTH/2 > this.x && player.y < this.y + HEIGHT/2 && player.y + HEIGHT/2 > this.y ) {
+        //碰撞检测
+        if (player.x < this.x + WIDTH / 2 && player.x + WIDTH / 2 > this.x && player.y < this.y + HEIGHT / 2 && player.y + HEIGHT / 2 > this.y) {
             console.log('撞了');
             player.again();
         }
@@ -38,14 +38,13 @@ class Enemy {
 }
 
 
-
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
 class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.sprite ='images/char-boy.png';
+        this.sprite = 'images/char-boy.png';
     }
     resetPlayer() {
         this.x = 2 * WIDTH;
@@ -59,26 +58,38 @@ class Player {
     }
     //玩家移动，不能越界。
     handleInput(key) {
-        switch(key) {
-            case 'left': this.x -= WIDTH; 
-            if (player.x<0 || player.y<0 || player.x > 4*WIDTH || player.y > 5*HEIGHT) {player.resetPlayer();}
-            break;
-            case 'right': this.x += WIDTH; 
-            if (player.x<0 || player.y<0 || player.x > 4*WIDTH || player.y > 5*HEIGHT) {player.resetPlayer();}
-            break; 
-            case 'up': this.y -= HEIGHT; 
-            if (player.x<0 || player.y<0 || player.x > 4*WIDTH || player.y > 5*HEIGHT) {player.resetPlayer();}
-            break;
-            case 'down': this.y += HEIGHT; 
-            if (player.x<0 || player.y<0 || player.x > 4*WIDTH || player.y > 5*HEIGHT) {player.resetPlayer();}
-            break;
+        switch (key) {
+            case 'left':
+                this.x -= WIDTH;
+                if (player.x < 0 || player.y < 0 || player.x > 4 * WIDTH || player.y > 5 * HEIGHT) {
+                    player.resetPlayer();
+                }
+                break;
+            case 'right':
+                this.x += WIDTH;
+                if (player.x < 0 || player.y < 0 || player.x > 4 * WIDTH || player.y > 5 * HEIGHT) {
+                    player.resetPlayer();
+                }
+                break;
+            case 'up':
+                this.y -= HEIGHT;
+                if (player.x < 0 || player.y < 0 || player.x > 4 * WIDTH || player.y > 5 * HEIGHT) {
+                    player.resetPlayer();
+                }
+                break;
+            case 'down':
+                this.y += HEIGHT;
+                if (player.x < 0 || player.y < 0 || player.x > 4 * WIDTH || player.y > 5 * HEIGHT) {
+                    player.resetPlayer();
+                }
+                break;
         }
-    }   
+    }
     again() {
         allEnemies = [];
         $('.congratulation').hide();
         player.resetPlayer();
-    } 
+    }
 
 }
 
@@ -86,16 +97,16 @@ class Player {
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
 var allEnemies = [];
-var player = new Player(2 * WIDTH,4 * HEIGHT);
-setInterval(function() {
-    var enemy = new Enemy(0,Math.floor(Math.random() * 4) * 83);
+var player = new Player(2 * WIDTH, 4 * HEIGHT);
+setInterval(function () {
+    var enemy = new Enemy(0, Math.floor(Math.random() * 4) * 83);
     allEnemies.push(enemy);
-},1000)
+}, 1000)
 
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Player.handleInput()
 // 方法里面。你不需要再更改这段代码了。
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
